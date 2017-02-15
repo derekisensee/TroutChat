4fdaef41f55043c384bbb9ac10a1c0b262e0c728
Derek Isensee <derek.isensee@gmail.com>
2017-02-15 08:41:47 -0600

    Initial stuff

--- TroutChat/Program.cs
+++ TroutChat/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Text;
 
 namespace TroutChat
 {
@@ -14,6 +16,7 @@ namespace TroutChat
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
